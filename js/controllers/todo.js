b8a3a7e612c954e5181fc942bb2c81ec31fd43a8
lemelon <lambertfelix8@gmail.com>
2015-12-30 18:04:21 +0100

    bonjour

--- js/controllers/todo.js
+++ js/controllers/todo.js
@@ -8,55 +8,14 @@
  */
 
 define([
-	'angular'
-], function (angular) {
+	'angular',
+	'cozydb_browser'
+], function (angular, cozydb_browser) {
 	return ['$scope', '$location', 'todoStorage', 'filterFilter',
 		function ($scope, $location, todoStorage, filterFilter) {
-			
-
 			$scope.addTodo = function () {
 				console.log('inside todo');
 				// cozydb_browser.escape();
-			};
-
-
-			$scope.editTodo = function (todo) {
-				$scope.editedTodo = todo;
-				// Clone the original todo to restore it on demand.
-				$scope.originalTodo = angular.copy(todo);
-			};
-
-
-			$scope.doneEditing = function (todo) {
-				$scope.editedTodo = null;
-				todo.title = todo.title.trim();
-
-				if (!todo.title) {
-					$scope.removeTodo(todo);
-				}
-			};
-
-			$scope.revertEditing = function (todo) {
-				todos[todos.indexOf(todo)] = $scope.originalTodo;
-				$scope.doneEditing($scope.originalTodo);
-			};
-
-			$scope.removeTodo = function (todo) {
-				todos.splice(todos.indexOf(todo), 1);
-			};
-
-
-			$scope.clearDoneTodos = function () {
-				$scope.todos = todos = todos.filter(function (val) {
-					return !val.completed;
-				});
-			};
-
-
-			$scope.markAll = function (done) {
-				todos.forEach(function (todo) {
-					todo.completed = done;
-				});
 			};
 		}
 	];
